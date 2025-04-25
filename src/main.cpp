c32c6955d8d5da3fa2dee02617ed41ca410ce739
BerrocalG <guadalupe.berrocal@usil.pe>
2025-04-24 19:33:05 -0500

    compilando matriz con display

--- src/main.cpp
+++ src/main.cpp
@@ -6,52 +6,52 @@
 int main(void) {
   DDRD|=0xF0; //matrizcolumna
   DDRD&= ~0X0F; //matriz fila
+  PORTD |= 0x0F;    // en up fila
 
   DDRB|=0X0F; //codificador
   DDRC|=0X07; //A0 A1 A2 display
   DDRC|=0X08; //A3 led
 
-  char cuenta=0;
-  char boton= 0;
+  char filas;
+  char secuencia;
+  char cuenta;
 
   while(1){
-  
-      PORTD|=0X0F; //prende filas
-      while (1){
+    //centenas display
+    PORTC&=~(0X01);
+    PORTC&=~(0X02);
+    PORTC|=0X04;
+    PORTB |= ((cuenta / 100) << 4);   //centenas
+    PORTB &= ((cuenta / 100) << 4) | 0x0F; 
+    _delay_ms(8);
+    //decenas display
+    PORTC&=~(0X01);
+    PORTC&=~(0X04);
+    PORTC|=0X02;
+    PORTB = (PORTB & 0x0F) | ((cuenta / 10) << 4);
 
-        PORTC&=~(0X01);
-        PORTC&=~(0X02);
-        PORTC|=0X04;
-        PORTB |= ((cuenta / 100) << 4);   //centenas
-        PORTB &= ((cuenta / 100) << 4) | 0x0F; 
-        _delay_ms(8);
+    _delay_ms(8);
+    //unidades display
+    PORTC&=~(0X04);
+    PORTC&=~(0X02);
+    PORTC|=0X01;
+    PORTB|=0x01;PORTB = (PORTB & 0xF0) | ((cuenta % 10) >> 4);
+    
+    _delay_ms(8);
 
-        PORTC&=~(0X01);
-        PORTC&=~(0X04);
-        PORTC|=0X02;
-        PORTB|=(((cuenta / 10) % 10) << 4);
-        PORTB&=(((cuenta / 10) % 10) << 4)| 0X0F;//decenas
+    PORTD |= 0xF0;    // Todas las columnas en high
+    PORTD &= ~0x10;   // Activar solo columna 0 (PD4 en LOW)
+    _delay_us(5);     // 
+    filas = PIND & 0x0F; // Leemos solo las filas (PD0–PD3)
+    switch (filas){
+      case 0x0E: // fila 1
+      if(secuencia==0){    
 
-        _delay_ms(8);
-        PORTC&=~(0X04);
-        PORTC&=~(0X02);
-        PORTC|=0X01;
-        PORTB|=((cuenta % 10) << 4);
-        PORTB&=((cuenta  % 10) << 4)| 0X0F;//unidades
-      _delay_ms(8);
-
-      PORTD|=0XF0;
-      PORTD&=~0X10;
-      boton= (!(PIND & 0XF0)) & 0XF0;
-      switch (boton){
-
-        case 0X10:
-        _delay_ms(100);
-        cuenta=1;        
-        break;
-
-
+        cuenta=1; // Enciende LED
+        
+        secuencia=1;
       }
+      break;
   }
 }
 }
